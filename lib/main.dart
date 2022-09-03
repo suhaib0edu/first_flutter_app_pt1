a8a0574b57618206ae22dd969f3b14fea132b428
Suhaib Eltayeb <suhab.edu@gmail.com>
2022-09-03 12:47:55 -0700

    تهيئة واجهة التطبيق

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
   }
 }
